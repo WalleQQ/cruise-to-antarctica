75f7e939413db2dba4d0f75a769db759d9874afd
Vitalii Dragulin <80743821+WalleQQ@users.noreply.github.com>
2022-03-10 17:16:21 +0400

    добавил блок reservation

--- source/js/menu.js
+++ source/js/menu.js
@@ -1,12 +1,13 @@
-const navigationContainer = document.querySelector(".main-nav");
-const openMenu = document.querySelector(".main-nav__toggle");
-const closeMenu = openMenu.querySelector("span");
-const menu = document.querySelector(".main-nav__menu");
+const navigationContainer = document.querySelector('.main-nav');
+const openMenu = document.querySelector('.main-nav__toggle');
+const closeMenu = openMenu.querySelector('span');
+const menu = document.querySelector('.main-nav__menu');
+const anchors = document.querySelectorAll('a');
 
-navigationContainer.classList.remove("main-nav--no-js");
+navigationContainer.classList.remove('main-nav--no-js');
 
-openMenu.addEventListener("click", () => {
-  openMenu.classList.toggle("main-nav__toggle--show");
-  menu.classList.toggle("main-nav__menu--show-menu");
-  closeMenu.classList.toggle("main-nav__toggle-item--close-menu");
+openMenu.addEventListener('click', () => {
+  openMenu.classList.toggle('main-nav__toggle--show');
+  menu.classList.toggle('main-nav__menu--show-menu');
+  closeMenu.classList.toggle('main-nav__toggle-item--close-menu');
 });
