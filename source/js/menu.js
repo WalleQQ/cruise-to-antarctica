e44644655f55893bde5411e7593f03333ab28ad4
Vitalii Dragulin <80743821+WalleQQ@users.noreply.github.com>
2022-03-12 01:48:06 +0400

    подогнал проект под pixel perfect

--- source/js/menu.js
+++ source/js/menu.js
@@ -2,12 +2,20 @@ const navigationContainer = document.querySelector('.main-nav');
 const openMenu = document.querySelector('.main-nav__toggle');
 const closeMenu = openMenu.querySelector('span');
 const menu = document.querySelector('.main-nav__menu');
-const anchors = document.querySelectorAll('a');
+const anchors = menu.querySelectorAll('a');
 
 navigationContainer.classList.remove('main-nav--no-js');
 
-openMenu.addEventListener('click', () => {
+openMenu.addEventListener('click', (evt) => {
   openMenu.classList.toggle('main-nav__toggle--show');
   menu.classList.toggle('main-nav__menu--show-menu');
   closeMenu.classList.toggle('main-nav__toggle-item--close-menu');
+});
+
+anchors.forEach((anchor) => {
+  anchor.addEventListener('click', () => {
+    openMenu.classList.remove('main-nav__toggle--show');
+    menu.classList.remove('main-nav__menu--show-menu');
+    closeMenu.classList.remove('main-nav__toggle-item--close-menu');
+  });
 });
