9fafc0fe35f61dca49b361dab3965bc135a09b9e
Vitalii Dragulin <80743821+WalleQQ@users.noreply.github.com>
2022-03-17 12:17:42 +0400

    добавил правки после код ревью

--- gulpfile.js
+++ gulpfile.js
@@ -26,7 +26,7 @@ gulp.task('css', function () {
     .pipe(csso())
     .pipe(rename('style.min.css'))
     .pipe(sourcemap.write('.'))
-    .pipe(gulp.dest('build/css'))
+    .pipe(gulp.dest('source/css'))
     .pipe(server.stream());
 });
 
