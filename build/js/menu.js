bb041a7494fc8d38fe7afaf62517dc6851a7b7f9
Vitalii Dragulin <80743821+WalleQQ@users.noreply.github.com>
2022-03-25 15:49:11 +0400

    обновил билд

--- build/js/menu.js
+++ build/js/menu.js
@@ -4,6 +4,7 @@ const closeMenu = openMenu.querySelector('span');
 const menu = document.querySelector('.main-nav__menu');
 const anchors = menu.querySelectorAll('a');
 const pageBody = document.querySelector('.page-body');
+const overlay = document.querySelector('.page-header__overlay');
 
 navigationContainer.classList.remove('main-nav--no-js');
 
@@ -12,6 +13,7 @@ openMenu.addEventListener('click', (evt) => {
   menu.classList.toggle('main-nav__menu--show-menu');
   closeMenu.classList.toggle('main-nav__toggle-item--close-menu');
   pageBody.classList.toggle('page-body__isOpenMenu');
+  overlay.classList.toggle('page-header__overlay--isShow');
 });
 
 anchors.forEach((anchor) => {
@@ -20,5 +22,14 @@ anchors.forEach((anchor) => {
     menu.classList.remove('main-nav__menu--show-menu');
     closeMenu.classList.remove('main-nav__toggle-item--close-menu');
     pageBody.classList.remove('page-body__isOpenMenu');
+    overlay.classList.remove('page-header__overlay--isShow');
   });
+});
+
+overlay.addEventListener('click', () => {
+  openMenu.classList.remove('main-nav__toggle--show');
+  menu.classList.remove('main-nav__menu--show-menu');
+  closeMenu.classList.remove('main-nav__toggle-item--close-menu');
+  pageBody.classList.remove('page-body__isOpenMenu');
+  overlay.classList.remove('page-header__overlay--isShow');
 });
